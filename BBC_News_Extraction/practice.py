06787e8021fcfedd483c826627881553a3fed2f8
V Sarath Chandra <saisarathchandra539@gmail.com>
2026-09-23 20:32:03 +0530

    Normalize embeddings and implement inner product similarity scoring

--- BBC_News_Extraction/practice.py
+++ BBC_News_Extraction/practice.py
@@ -1,19 +1,32 @@
 import chromadb
+import numpy as np
 
-client = chromadb.PersistentClient(path="/news/chroma_db")
+
+path = 'D:\AI Projects\RAG Practice\BBC_News_Extraction\chroma_db'
+client = chromadb.PersistentClient(path=path)
 
 # # Get a list of all collections
 # collections = client.list_collections()
-
-# # Print the names of the collections
 # for collection in collections:
 #     print(collection.name)
 
+# collection = client.create_collection(
+#     name="bbc_news",
+#     metadata={"hnsw:space": "ip"},
+# )
 
 collection = client.get_collection("bbc_news")
 
 print(collection.count())
 
+# Check if the collection uses inner product method for similarity search
+metric = "l2 (default)"
+if collection.metadata and "hnsw:space" in collection.metadata:
+    metric = collection.metadata["hnsw:space"]
+
+print(f"The active distance metric for this collection is: '{metric}'")
+
+# Fetch top 5 documents from the collection
 data = collection.get(
     limit=5,
     include=["embeddings", "documents", "metadatas"]
@@ -22,9 +35,8 @@ data = collection.get(
 embeddings = data['embeddings'][0]
 print(len(embeddings))
 
-for i in range(5):
-    print(f"ID: {data['ids'][i]}")
-    print(f"Document: {data['documents'][i]}")
-    print(f"Metadata: {data['metadatas'][i]}")
-    print(f"Embedding: {data['embeddings'][i]}")
-    print("-" * 80)
+# Check if the embeddings are normalized
+print("Embeddings magnitudes:")
+for ind, embedding in enumerate(data["embeddings"]):
+    magnitude = np.linalg.norm(embedding)
+    print(f"Document-{ind+1}: {magnitude}")
